6804091c7c7de1e4139c5c254672ad4374e03c3d
berkaykrc <berkay.kirci@gmail.com>
2023-11-10 17:02:22 +0300

    Add filter buttons to main screen

--- src/screens/main/styles.js
+++ src/screens/main/styles.js
@@ -3,8 +3,7 @@ import { StyleSheet } from "react-native";
 export default styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
+      backgroundColor: '#fff',
     },
     button: {
       position: "absolute",
@@ -17,5 +16,33 @@ export default styles = StyleSheet.create({
       alignItems: "center",
       justifyContent: "center",
       elevation: 5,
+    },
+    filters: {
+      flexDirection: 'row',
+      padding: 10,
+      justifyContent: 'space-around',
+    },
+    filterButton: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 12,
+      paddingHorizontal: 15,
+      borderRadius: 15,
+      borderColor:'#000',
+      borderWidth: 1,
+    },
+    activeFilter: {
+      backgroundColor: '#000',
+    },
+    inactiveFilter: {
+      backgroundColor: '#fff',
+    },
+    activeText: {
+      color: '#fff',
+      fontSize: 16,
+    },
+    inactiveText: {
+      color: '#000',
+      fontSize: 16,
     },
   });
